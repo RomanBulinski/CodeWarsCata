fb06746f1f0b13ee0c0b022a004474445c285666
rbulin <roman.bulinski@pentacomp.pl>
2020-02-29 17:02:34 +0100

    Implemented code

--- src/main/java/Katas.java
+++ src/main/java/Katas.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Katas {
 
+    public static int[] pendulum(final int[] values) {
+
+        int[] arr = values;
+        Arrays.sort(arr);
+        boolean check = true;
+        List<Integer> result = new ArrayList<>();
+        result.add( arr[0] );
+        for(int i =1; i<arr.length; i++){
+            if( i<arr.length && check == true ){
+                result.add( arr[i]);
+            }
+            i++;
+            check =false;
+            if( i<arr.length && check == false ){
+                result.add( 0,arr[i]);
+            }
+            check =true;
+         }
+        return result.stream().mapToInt(x->x).toArray();
+    }
 
 }
